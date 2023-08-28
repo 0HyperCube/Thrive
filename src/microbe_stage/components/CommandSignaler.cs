631d4bfb2f1247e0442993e7c0dca91008558240
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2023-08-28 10:57:04 +0300

    Created a dedicated signalling command system

--- src/microbe_stage/components/CommandSignaler.cs
+++ src/microbe_stage/components/CommandSignaler.cs
@@ -1,21 +1,36 @@
 ﻿namespace Components
 {
+    using DefaultEcs;
     using Godot;
 
     /// <summary>
-    ///   Sends and receivers command signals (signaling agent)
+    ///   Sends and receivers command signals (signalling agent). Requires a <see cref="WorldPosition"/> to function
+    ///   as the origin of the signalling command.
     /// </summary>
     public struct CommandSignaler
     {
-        // TODO: system to update
-        public Vector3 EmittedPosition;
-
+        /// <summary>
+        ///   Stores the position the command signal was received from. Only valid if <see cref="ReceivedCommand"/> is
+        ///   not <see cref="MicrobeSignalCommand.None"/>.
+        /// </summary>
         public Vector3 ReceivedCommandSource;
 
         /// <summary>
-        ///   Because AI is ran in parallel thread, if it wants to change the signaling, it needs to do it through this
+        ///   Entity that sent the detected signal. Not valid if <see cref="ReceivedCommand"/> is not set (see
+        ///   documentation on <see cref="ReceivedCommandSource"/>).
         /// </summary>
-        public MicrobeSignalCommand? QueuedSignalingCommand;
+        public Entity ReceivedCommandFromEntity;
+
+        /// <summary>
+        ///   Used to limit signals reaching entities they shouldn't. In the microbe stage this contains the entity's
+        ///   species ID to allow species-wide signalling.
+        /// </summary>
+        public ulong SignallingChannel;
+
+        /// <summary>
+        ///   Because AI is ran in parallel thread, if it wants to change the signalling, it needs to do it through this
+        /// </summary>
+        public MicrobeSignalCommand? QueuedSignallingCommand;
 
         public MicrobeSignalCommand Command;
 
